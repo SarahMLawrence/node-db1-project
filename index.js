b4104b6372f27f483da84a43598ef72d97d77068
Sarah Lawrence <sarahlawrence1152@gmail.com>
2020-08-19 14:47:49 -0400

    CRUD complete

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const server = require("./api/server.js");
+const accountRouter = require("./accounts/account-router");
+const welcomeRouter = require("./welcome/welcome-router");
 
 const PORT = process.env.PORT || 5000;
+
+server.use("/", welcomeRouter)
+server.use("/accounts", accountRouter)
+
 
 server.listen(PORT, () => {
   console.log(`\n== API running on port ${PORT} ==\n`);
